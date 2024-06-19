68d288c0234223da63fe62029c17ecdd0e6fddfa
Viktoriia Lavrova <viktoriia.lav2908@gmail.com>
2024-06-19 21:44:40 +0300

    add function print_kuku

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def print_hi(name):
 def print_bye(name):
     print(f'Bye, {name}!')
 
+
+def print_kuku(name):
+    print(f'Ku-ku, {name}!')
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
